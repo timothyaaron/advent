dcfb39ebb839412f991573c76be4117b956c04f9
Timothy Cloninger <timothy.cloninger@life.church>
2022-12-06 22:06:35 -0600

    Day 3, take 2

--- 03/answer.py
+++ 03/answer.py
@@ -1,18 +1,16 @@
 priority = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+def unique(first, *the_rest):
+    """turn 2+ lists in sets, intersect, and return the found character"""
+    return set(first).intersection(*map(set, the_rest)).pop()
+
 with open("input.txt") as f:
-    rucksacks = [list(line.strip()) for line in f]
+    sacks = [list(line.strip()) for line in f]
 
 # split the lists in half, set+intersection to find the unique character
-items = [
-    (set(r[:len(r)//2]) & set(r[len(r)//2:])).pop()
-    for r in rucksacks
-]
+items = [unique(sack[:len(sack)//2], sack[len(sack)//2:]) for sack in sacks]
 print(sum([priority.index(i) for i in items]))
 
-# loop over every 3 rucksacks, set+intersection the unique character
-items = [
-    (set(rucksacks[3*i]) & set(rucksacks[3*i+1]) & set(rucksacks[3*i+2])).pop()
-    for i in range(len(rucksacks)//3)
-]
+# loop over every 3 sacks, set+intersection to find the unique character
+items = [unique(sacks[3*i], sacks[3*i+1], sacks[3*i+2]) for i in range(len(sacks)//3)]
 print(sum([priority.index(i) for i in items]))
