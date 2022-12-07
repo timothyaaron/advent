4b707c721b7e5317a66e8f6919eb9ae2b8fc14e7
Timothy Cloninger <timothy.cloninger@life.church>
2022-12-06 21:47:12 -0600

    Day 4, take 2

--- 04/answer.py
+++ 04/answer.py
@@ -1,20 +1,14 @@
+def to_set(assignment):
+    """e.g., turn the string "2-4" into the set {2,3,4}"""
+    start, end = [int(x) for x in assignment.split("-")]
+    return set(range(start, end + 1))
+
 with open("input.txt") as f:
-    assignments = [line.strip().split(",") for line in f]  # list of ["2-4", "3-4"]
+    """e.g., turn each "2-4,4-5" into a pair of sets [{2,3,4},{4,5}]"""
+    assignments = [[to_set(a) for a in line.strip().split(",")] for line in f]
 
-complete_overlaps = []
-partial_overlaps = []
-for a1, a2 in assignments:
-    # turn each assignment ("2-4") into a set of integers ({2,3,4})
-    a1_sections = {i for i in range(int(a1.split("-")[0]), int(a1.split("-")[1]) + 1)}
-    a2_sections = {i for i in range(int(a2.split("-")[0]), int(a2.split("-")[1]) + 1)}
-
-    # add to `complete` if either set is a subset of the other
-    if a1_sections.issubset(a2_sections) or a2_sections.issubset(a1_sections):
-        complete_overlaps.append([a1, a2])
-
-    # add to `partial` if no sections appear in both sets
-    if not a1_sections.isdisjoint(a2_sections):
-        partial_overlaps.append([a1, a2])
+complete_overlaps = [(a1, a2) for a1, a2 in assignments if a1 <= a2 or a2 <= a1]
+partial_overlaps = [(a1, a2) for a1, a2 in assignments if not a1.isdisjoint(a2)]
 
 print(f"Complete Overlays:\n {len(complete_overlaps)}\n")
 print(f"Partial Overlays:\n {len(partial_overlaps)}\n")
