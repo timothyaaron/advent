927023ed1f592fab44bed6563a069592c9b4189f
Timothy Cloninger <timothy.cloninger@life.church>
2022-12-08 20:18:52 -0600

    Day 8, take 2

--- 08/answer.py
+++ 08/answer.py
@@ -1,11 +1,13 @@
+import numpy
+
+
 hidden_tree_coords = []
 scenic_scores = []
 
 with open("input.txt") as f:
-    forest = [[tree for tree in row] for row in f.read().splitlines()]
+    forest = numpy.array([[tree for tree in row] for row in f.read().splitlines()])
 
-
-def check_hidden(x, y):
+def append_if_hidden(x, y):
     # Look for a tree that will hide this one, from each direction
     # ... generator to prevent loading entire range
     # ... `next` bails at first find; `and` shortcuts if visible from one direction
@@ -20,36 +22,29 @@ def check_hidden(x, y):
 
 def score_scenicality(x, y):
     """In each direction, from the tree to the border, increment until blocked, multiply."""
-    north = south = east = west = 0
-    height = forest[y][x]
 
-    for tree in forest[y][0:x][::-1]:
-        west += 1
-        if tree >= height:
-            break
+    def count_trees(range):
+        count = 0
+        for tree in range:
+            count += 1
+            if tree >= forest[y][x]:
+                break
+        return count
 
-    for tree in forest[y][x + 1:len(forest[0])]:
-        east += 1
-        if tree >= height:
-            break
-
-    for j in range(y)[::-1]:
-        north += 1
-        if forest[j][x] >= height:
-            break
-
-    for j in range(y + 1, len(forest)):
-        south += 1
-        if forest[j][x] >= height:
-            break
-
-    scenic_scores.append(west * east * south * north)
+    ranges = [
+        forest[y, :x][::-1],  # west
+        forest[y, x + 1:len(forest[0])],  # east
+        forest[:y, x][::-1],  # north
+        forest[y + 1:len(forest), x],  # south
+    ]
+    trees = [count_trees(range) for range in ranges]
+    scenic_scores.append(numpy.prod(trees))
 
 
 # for every tree inside the border…
 for x in range(1, len(forest[0]) - 1):
     for y in range(1, len(forest) - 1):
-        check_hidden(x, y)
+        append_if_hidden(x, y)
         score_scenicality(x, y)
 
 print((len(forest) * len(forest[0])) - len(hidden_tree_coords))
