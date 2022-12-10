6dee41aa8901699907c1325768fd690299f3e585
Timothy Cloninger <timothy.cloninger@life.church>
2022-12-10 00:45:22 -0600

    Day 10, part 2

--- 10/answer.py
+++ 10/answer.py
@@ -1,22 +1,55 @@
+DEBUG = False  # watch screen print
+
 with open("input.txt") as f:
     instructions = [row.split() for row in f.read().splitlines()]
 
 x = 1
 cycle = 1
 sig_str = []
-for i in instructions:
+output = [""]
 
+for i in instructions:
+    if DEBUG:
+        print(i)
+
+    # output, then close noop and addx (part 1) cycles
+    output[-1] += "#" if (cycle-1) % 40 in (x - 1, x, x + 1) else "."
     cycle += 1
     if (cycle + 20) % 40 == 0:
-        sig_str.append((cycle, x))
+        sig_str.append((cycle * x))
+    if len(output[-1]) == 40:
+        output.append("")
+
+    if DEBUG:
+        for line in output:
+            print(line)
+
+        print(("." * (x-1)) + ("#" * 3) + "\n")
+        input()
+
 
     if i[0] == "addx":
+        # addx, part2
+        output[-1] += "#" if (cycle-1) % 40 in (x - 1, x, x + 1) else "."
         cycle += 1
+        if len(output[-1]) == 40:
+            output.append("")
+
+        if DEBUG:
+            for line in output:
+                print(line)
+
+            print(("." * (x-1)) + ("#" * 3))
+
         x += int(i[1])
-
         if (cycle + 20) % 40 == 0:
-            sig_str.append((cycle, x))
+            sig_str.append((cycle * x))
 
-print(sig_str)
-print([x * c for x, c in sig_str])
-print(sum([x * c for x, c in sig_str]))
+        if DEBUG:
+            print(("." * (x-1)) + ("#" * 3) + "\n")
+            input()
+
+print(sum(sig_str))
+
+for line in output:
+    print(line)
